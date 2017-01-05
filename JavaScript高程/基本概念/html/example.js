460606982f4d75e1c694c5fc559e7f8747612b7a
fortress-fighting <dreamersfly@163.com>
2017-01-06 01:07:25 +0800

    基本概念

---
+++ JavaScript高程/基本概念/html/example.js
@@ -0,0 +1,13 @@
+// console.log(isNaN(''));
+
+// var num = 10;
+// console.log(num--); // 10
+// console.log(num); // 9
+// var num1 = 10;
+// console.log(--num1); // 9
+
+// ------------------一元加减操作符
+
+var a = '12';
+console.log(+a); // 12
+console.log(typeof(+a)); // number
