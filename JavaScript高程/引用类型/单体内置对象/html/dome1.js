5e5d0a47615245411f108ecc91790b21aef55457
fortress-fighting <dreamersfly@163.com>
2017-01-15 20:11:43 +0800

    添加引用类型

---
+++ JavaScript高程/引用类型/单体内置对象/html/dome1.js
@@ -0,0 +1,3 @@
+var u = 'https://www.lagou.com/resume/my resume.html'
+console.log(encodeURI(u)); // https://www.lagou.com/resume/my%20resume.html
+console.log(encodeURIComponent(u)); // https%3A%2F%2Fwww.lagou.com%2Fresume%2Fmy%20resume.html
